fcd7a5813c523fd43e67095628391c90c9858ceb
sudovaibhav <vaibhav.b.g.patil2@gmail.com>
2025-10-18 10:04:04 +0530

    Added a feature to calculate circumference of circle

--- file.py
+++ file.py
@@ -1,3 +1,4 @@
 radius = int(input("Enter the radius of the cirlce: "))
 area = 3.142 * radius ** 2
-print(f"Area of Circle is {area}")
+circumference = 2 * 3.142 * radius
+print(f"Area of Circle is {area}\nCircumference of Circle is {circumference}")
